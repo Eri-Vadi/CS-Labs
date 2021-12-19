45cb3a3cf174919fe411c0e3cad0d169273b6536
Eric <90966216+Eri-Vadi@users.noreply.github.com>
2021-12-19 14:46:07 +0200

    Add files via upload

---
+++ Lab7/LabCsMongoDB.Data/Implementation/UsersRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using LabCsMongoDB.Data.Encryption;
+using LabCsMongoDB.Data.Models;
+
+namespace LabCsMongoDB.Data.Implementation
+{
+     public class UsersRepository : MongoRepository<User>
+     {
+          private Encryptor Encryptor { get; set; }
+          public UsersRepository() : base()
+          {
+               var password = GetPassword();
+               Encryptor = new Encryptor(password);
+          }
+
+          public async Task AddUserAndEncryptData(User user)
+          {
+               var userWithEncryptedData = new User
+               {
+                    Id = default,
+                    Name = user.Name,
+                    Password = Encryptor.Encrypt(user.Password),
+                    CardNumber = Encryptor.Encrypt(user.CardNumber),
+                    Email = Encryptor.Encrypt(user.Email),
+               };
+
+               await InsertOneAsync(userWithEncryptedData);
+          }
+
+          public List<User> GetUsersWithEncryptedData()
+          {
+               var usersEncrypted = AsQueryable().ToList();
+               var usersDecrypted = usersEncrypted.Select(x => new User
+               {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Password = Encryptor.Decrypt(x.Password),
+                    CardNumber = Encryptor.Decrypt(x.CardNumber),
+                    Email = Encryptor.Decrypt(x.Email)
+               }).ToList();
+               return usersDecrypted;
+          }
+
+          private static SecureString GetPassword()
+          {
+               var password = new SecureString();
+
+               password.AppendChar((char)80); // P
+               password.AppendChar((char)97); // a
+               password.AppendChar((char)115); // s
+               password.AppendChar((char)115); // s
+               password.AppendChar((char)119); // w
+               password.AppendChar((char)111); // o
+               password.AppendChar((char)114); // r
+               password.AppendChar((char)100); // d
+               password.AppendChar((char)49); // 1
+
+               return password;
+          }
+     }
+}
---
+++ Lab7/LabCsMongoDB.UI/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Mail;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using LabCsMongoDB.Data.Implementation;
+using LabCsMongoDB.Data.Models;
+
+namespace LabCsMongoDB.UI
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          public List<User> Data { get; set; }
+          private UsersRepository UsersRepository { get; set; }
+          public MainWindow()
+          {
+               Data = new List<User>();
+               UsersRepository = new UsersRepository();
+               InitializeComponent();
+          }
+
+          private void FetchDataHandler(object sender, RoutedEventArgs e)
+          {
+               Data = UsersRepository.GetUsersWithEncryptedData();
+               RenderTasks();
+          }
+
+          private void RenderTasks()
+          {
+               UserList.Items.Clear();
+               foreach (var user in Data)
+               {
+                    UserList.Items.Add(new TextBlock()
+                    {
+                         Text =
+                              $"Id: {user.Id}, Name: {user.Name}, Email: {user.Email}, Password: {user.Password}, Card Number: {user.CardNumber}"
+                    });
+               }
+          }
+
+          private async void AddTaskHandler(object sender, RoutedEventArgs e)
+          {
+               await UsersRepository.AddUserAndEncryptData(new User
+               {
+                    Id = default,
+                    Name = "Bill Gates",
+                    Password = "gatesMicrosoftTheBest",
+                    CardNumber = "9942-3662-2222-0007",
+                    Email = "bill.the.best@gmail.com",
+               });
+               MessageBox.Show("User added.");
+          }
+     }
+}
